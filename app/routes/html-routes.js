5d43139a60d4e97c257cd6be6e1f130dfae8cfa8
matt <xxmaker809xx@gmail.com>
2020-01-18 11:38:41 -0500

    built server and put together some of the basic routes

---
+++ app/routes/html-routes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = function (app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // index route loads view.html
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../login.html"));
+    });
+
+    app.get("/register", function (req, res) {
+        res.sendFile(path.join(__dirname, "../registration.html"));
+    });
+
+    app.get("/forgot", function (req, res) {
+        res.sendFile(path.join(__dirname, "../forgotpassword.html"));
+    });
+
+};
